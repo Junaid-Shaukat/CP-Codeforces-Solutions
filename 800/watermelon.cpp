b11da6ead04b8d34f4f23b33712d066296321e29
Junaid <junaiddshaukat@gmail.com>
2024-09-30 19:10:58 +0500

    one 800 problem solved

---
+++ 800/watermelon.cpp
@@ -0,0 +1,19 @@
+// problem link : https://codeforces.com/contest/4/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+int w;
+cin>>w;
+if(w==0) cout<<"NO";
+else if(w%2==0){
+    cout<<"YES";
+}
+else{
+    cout<<"NO";
+}
+ 
+return 0;
+	
+}
