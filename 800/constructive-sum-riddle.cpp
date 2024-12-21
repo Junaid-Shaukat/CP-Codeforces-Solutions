3504e38fc8284f10a9e6b34005396829f49ca0a2
Junaid-Shaukat <junaiddshaukat@gmail.com>
2024-12-21 09:54:14 +0500

    constructive-sum-riddle

---
+++ 800/constructive-sum-riddle.cpp
@@ -0,0 +1,16 @@
+// Problem Link : https://codeforces.com/contest/1594/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(void){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<-n+1<<" "<<n<<endl;
+    }
+}
